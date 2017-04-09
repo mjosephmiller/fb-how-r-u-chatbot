0a0124156a05a2a5e3d5608976aaa94b2e6acaad
Michael Miller <michael.miller@bilendo.de>
2017-04-09 23:42:08 +0200

    introduces Google NLP

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -1,29 +1,38 @@
 require "facebook/messenger"
 include Facebook::Messenger
+require "google/cloud/language"
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["FACEBOOK_ACCESS_TOKEN"])
+
 Bot.on :message do |message|
+    unless check_positive?(message)
+        reply(message.sender, "I can't deal with your negativity right now.")
+    end
     if %w(hello hi hiya yo hey).include? message.messaging['message']['text']
-        message.reply(text: 'Hello, humanoid!')
-    elsif %("how's it going" ok? alright?).include? message.messaging['message']['text']
-        message.reply(text: "I'm ok. How are you?")
-    elsif message.messaging['message']['text'] == "I'm good thanks!"
-        message.reply(text: "Good to hear. Ta-Ra for now.")
+        reply(message.sender, 'hello humanoid. How r u?')
+    elsif %("how's it going" ok? alright? What about you?).include? message.messaging['message']['text']
+       reply(message.sender, "Well it's #{Date.today.wday}, so I'm alright, thanks.")
+    else
+        reply(message.sender, 'g2g.')
     end
 end
 
-def sender
-    message.messaging["sender"]["id"]
+
+def reply(recipient, text)
+    Bot.deliver({
+        recipient: recipient,
+        message: {
+          text: text
+        }
+    }, access_token: ENV["FACEBOOK_ACCESS_TOKEN"])
 end
 
-def recipient
-    message.messaging["recipient"]["id"]
-end
-
-def reply(message)
-    payload = {
-    recipient: sender,
-    message: message
-    }
-    Facebook::Messenger::Bot.deliver(payload, access_token: ENV["ACCESS_TOKEN"])
+def check_positive?(message)
+    project_id = ENV["NLP_GOOGLE_APP_ID"]
+    language = Google::Cloud::Language.new project: project_id
+    text = message.messaging['message']['text']
+    document = language.document text
+    sentiment = document.sentiment
+    sentiment.score
+    sentiment.score > 0
 end
